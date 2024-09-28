80bd92ecd1c5f0e274e0b0dc5c14c8c0c243ffb7
Chris Roach <c.roach10@yahoo.com>
2024-09-28 14:10:33 -0400

    check in

--- AspireWithAngular.AppHost/obj/Debug/net8.0/AspireWithAngular.AppHost.AssemblyInfo.cs
+++ AspireWithAngular.AppHost/obj/Debug/net8.0/AspireWithAngular.AppHost.AssemblyInfo.cs
@@ -12,19 +12,19 @@ using System;
 using System.Reflection;
 
 [assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("bcaa3a76-2522-4b37-8625-aa648ad76373")]
-[assembly: System.Reflection.AssemblyMetadata("dcpclipath", "C:\\Program Files\\dotnet\\packs\\Aspire.Hosting.Orchestration.win-x64\\8.1.0\\tools\\dc" +
-    "p.exe")]
-[assembly: System.Reflection.AssemblyMetadata("dcpextensionpaths", "C:\\Program Files\\dotnet\\packs\\Aspire.Hosting.Orchestration.win-x64\\8.1.0\\tools\\ex" +
-    "t\\")]
-[assembly: System.Reflection.AssemblyMetadata("dcpbinpath", "C:\\Program Files\\dotnet\\packs\\Aspire.Hosting.Orchestration.win-x64\\8.1.0\\tools\\ex" +
-    "t\\bin\\")]
+[assembly: System.Reflection.AssemblyMetadata("dcpclipath", "C:\\Users\\chris\\.nuget\\packages\\aspire.hosting.orchestration.win-x64\\8.2.1\\tools\\d" +
+    "cp.exe")]
+[assembly: System.Reflection.AssemblyMetadata("dcpextensionpaths", "C:\\Users\\chris\\.nuget\\packages\\aspire.hosting.orchestration.win-x64\\8.2.1\\tools\\e" +
+    "xt\\")]
+[assembly: System.Reflection.AssemblyMetadata("dcpbinpath", "C:\\Users\\chris\\.nuget\\packages\\aspire.hosting.orchestration.win-x64\\8.2.1\\tools\\e" +
+    "xt\\bin\\")]
 [assembly: System.Reflection.AssemblyMetadata("apphostprojectpath", "C:\\Users\\chris\\source\\repos\\AspireWithAngular\\AspireWithAngular.AppHost")]
-[assembly: System.Reflection.AssemblyMetadata("aspiredashboardpath", "C:\\Program Files\\dotnet\\packs\\Aspire.Dashboard.Sdk.win-x64\\8.1.0\\tools\\Aspire.Das" +
-    "hboard.exe")]
+[assembly: System.Reflection.AssemblyMetadata("aspiredashboardpath", "C:\\Users\\chris\\.nuget\\packages\\aspire.dashboard.sdk.win-x64\\8.2.1\\tools\\Aspire.Da" +
+    "shboard.exe")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("AspireWithAngular.AppHost")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1cad732c3eb43373295cfd448feb3f3c669a442f")]
 [assembly: System.Reflection.AssemblyProductAttribute("AspireWithAngular.AppHost")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AspireWithAngular.AppHost")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
